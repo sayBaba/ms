0451c1add2535278f70cd940236b54e70e827c56
sayBaba <answer123@foxmail.com>
2019-08-30 18:55:08 +0800

    抢购项目

---
+++ src/main/java/com/hz/ms/utils/DruidUtile.java
@@ -0,0 +1,18 @@
+//package com.hz.ms.utils;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.datasource")
+//public class DruidUtile {
+//
+//    @Bean
+//    public static DruidDataSource getDruidDataSource(){
+//        return new DruidDataSource();
+//    }
+//
+//
+//}
